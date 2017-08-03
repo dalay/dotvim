9a6856ae8f799ec1c8c522ebf31f14fed783351d
dalay <dalay@drupalka.ru>
2017-08-04 02:31:28 +0300

    install script upd

--- install_config.sh
+++ install_config.sh
@@ -6,8 +6,6 @@ VIM_DIR=~/.vim
 
 ln -s $VIM_DIR/.vimrc ~/.vimrc
 
-cd $VIM_DIR
-git submodule update --init && vim -c 'PluginInstall' -c 'qa!'
+cd $VIM_DIR && git submodule update --init && vim -c 'PluginInstall' -c 'qa!'
 
-cd $VIM_DIR/bundle/Vundle.vim/·
-git checkout master
+cd $VIM_DIR/bundle/Vundle.vim/ && git checkout master
