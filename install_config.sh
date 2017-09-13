93ea79a61c839b04beab44857e084fa14f7f0b5c
dalay <dalay@drupalka.ru>
2017-09-13 12:47:54 +0300

    install script upd

--- install_config.sh
+++ install_config.sh
@@ -4,5 +4,5 @@ CUR_DIR=`pwd`
 VIM_DIR=~/.vim
 
 ln -s $VIM_DIR/.vimrc ~/.vimrc
-vim -c ':PlugInstall --sync | source $MYVIMRC' -c ':qa!<CR>'
+vim -c ':PlugInstall --sync | source $MYVIMRC' -c ':qa!'
 cd $CUR_DIR
